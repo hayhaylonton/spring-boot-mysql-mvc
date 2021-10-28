7595271d7231d251f50f62e423205d925829c9d4
bao.ly <baobaobonbon@gmail.com>
2021-10-28 22:17:42 +0700

    edit

--- src/main/java/com/example/accessingdatamysql/Application.java
+++ src/main/java/com/example/accessingdatamysql/Application.java
@@ -14,26 +14,4 @@ public class Application {
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
 	}
-
-	@Bean
-	public CommandLineRunner commandLineRunner(ApplicationContext ctx) {
-		
-
-		return args -> {
-			System.out.println("Let's inspect the beans provided by Spring Boot:");
-			
-		};
-	}
-
-	@Bean
-	public CommandLineRunner demo(UserRepository repository) {
-		return (args) -> {
-			//User n = new User();
-			//n.setName("bao.ly");
-			//n.setEmail("bao.ly@a.com");
-			//repository.save(n);
-			System.out.println("Let's inspect the beans provided by Spring Boot:");
-		};
-	}
-
 }
